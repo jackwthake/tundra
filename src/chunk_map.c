45d05d0e6d4e1f6bba7d965ac0dbe7db514a4d58
jackwthake <jackthake@hotmail.com>
2025-09-27 02:53:18 -0700

    Implemented Hash Map for loaded chunks

---
+++ src/chunk_map.c
@@ -0,0 +1,163 @@
+#include "chunk_map.h"
+
+#include <stdlib.h>
+
+static inline usize get_chunk_hash(usize x, usize z, usize table_size) {
+  return ((x * 73856093) ^ (z * 19349663)) % table_size;
+}
+
+static void free_chunk_node(chunk_map_node_t *node) {
+  if (!node) return;
+
+  delete_model(&node->chunk.ground_plane);
+  
+  for (usize i = 0; i < node->chunk.num_trees; ++i) {
+    delete_model(&node->chunk.trees[i]);
+  }
+
+  free(node);
+}
+
+void init_chunk_map(chunk_map_t *map, usize num_buckets) {
+  if (!map) return;
+
+  map->num_buckets = num_buckets;
+  map->buckets = calloc(num_buckets, sizeof(chunk_map_node_t *));
+}
+
+void free_chunk_map(chunk_map_t *map) {
+  if (!map) return;
+
+  for (usize i = 0; i < map->num_buckets; ++i) {
+    chunk_map_node_t *head = map->buckets[i];
+
+    while (head) {
+      chunk_map_node_t *next = head->next;
+      free_chunk_node(head);
+
+      head = next;
+    }
+  }
+
+  free(map->buckets);
+  map->buckets = NULL;
+}
+
+void insert_chunk(chunk_map_t *map, chunk_t *chunk) {
+  if (!map || !chunk) return;
+
+  usize index = get_chunk_hash(chunk->x, chunk->z, map->num_buckets);
+
+  chunk_map_node_t *old_head = map->buckets[index];
+
+  // emplace new chunk at start of list, no reason to iterate to the end to add
+  chunk_map_node_t *head = malloc(sizeof(chunk_map_node_t));
+  head->chunk = *chunk;
+  head->loaded = true;
+  head->next = old_head;
+
+  map->buckets[index] = head;
+}
+
+void remove_chunk(chunk_map_t *map, usize x, usize z) {
+  (void)map; (void)x; (void)z;
+  if (!map) return;
+
+  usize index = get_chunk_hash(x, z, map->num_buckets);
+  chunk_map_node_t *head = map->buckets[index], *prev = NULL;
+
+  while (head) {
+    if (head->chunk.x == x && head->chunk.z == z) {
+      if (prev == NULL) {
+        map->buckets[index] = head->next;
+      } else {
+        prev->next = head->next;
+      }
+
+      free_chunk_node(head);
+      return;
+    }
+    
+    prev = head;
+    head = head->next;
+  }
+}
+
+chunk_map_node_t *chunk_lookup(chunk_map_t *map, usize x, usize z) {
+  if (!map) return NULL;
+
+  usize index = get_chunk_hash(x, z, map->num_buckets);
+  chunk_map_node_t *head = map->buckets[index];
+
+  while (head) {
+    if (head->chunk.x == x && head->chunk.z == z)
+      return head;
+    
+    head = head->next;
+  }
+
+  return NULL;
+}
+
+bool is_chunk_loaded(chunk_map_t *map, usize x, usize z) {
+  return chunk_lookup(map, x, z)->loaded;
+}
+
+// used to get all chunks using query_chunk_map
+static inline bool always_true(chunk_t *chunk) {
+  (void)chunk;
+  return true;
+}
+
+void get_all_chunks(chunk_map_t *map, chunk_t **chunk_buf, usize *count) {
+  query_chunk_map(map, chunk_buf, count, always_true);
+}
+
+void query_chunk_map(chunk_map_t *map, chunk_t **chunk_buf, usize *count, query_func func) {
+  if (!map || !chunk_buf || !count) return;
+
+  *count = 0;
+  for (usize i = 0; i < map->num_buckets; ++i) {
+    chunk_map_node_t *node = map->buckets[i];
+    while (node != NULL) {
+      if (node->loaded && func(&node->chunk)) {
+        chunk_buf[*count] = &node->chunk;
+        (*count)++;
+      }
+      node = node->next;
+    }
+  }
+}
+
+#ifdef CHUNK_HASH_TEST
+int main(int argc, char const *argv[]) {
+  (void)argc; (void)argv;
+
+  chunk_map_t map = { 0 };
+
+  init_chunk_map(&map, CHUNK_MAP_NUM_BUCKETS);
+
+  chunk_t chunk = {
+    .x = 69,
+    .z = 420
+  };
+
+  insert_chunk(&map, &chunk);
+
+  chunk_map_node_t *chunk2 = chunk_lookup(&map, 69, 420);
+
+  chunk_t **chunks = calloc(map.num_buckets, sizeof(chunk_t *) * MAX_CHUNKS);
+  int count = 0;
+
+  get_all_chunks(&map, chunks, &count);
+
+  remove_chunk(&map, 69, 420);
+
+  chunk2 = chunk_lookup(&map, 69, 420);
+
+  free_chunk_map(&map);
+  free(chunks);
+
+  return 0;
+}
+#endif
---
+++ src/chunk_map.h
@@ -0,0 +1,43 @@
+#ifndef __CHUNK_MAP_H__
+#define __CHUNK_MAP_H__
+
+#include <shader-works/primitives.h>
+
+#define CHUNK_MAP_NUM_BUCKETS 16
+#define MAX_CHUNK_DISTANCE 2
+#define MAX_CHUNKS MAX_CHUNK_DISTANCE * 9
+
+typedef struct {
+  usize x, z;
+  model_t ground_plane;
+  model_t *trees;
+  usize num_trees;
+} chunk_t;
+
+typedef struct chunk_map_node_t {
+  chunk_t chunk;
+  struct chunk_map_node_t *next;
+  bool loaded;
+} chunk_map_node_t;
+
+typedef struct {
+  chunk_map_node_t **buckets;
+  usize num_buckets;
+} chunk_map_t;
+
+// return true to include chunk in final chunk buffer
+typedef bool (*query_func)(chunk_t *chunk);
+
+void init_chunk_map(chunk_map_t *map, usize num_buckets);
+void free_chunk_map(chunk_map_t *map);
+
+void insert_chunk(chunk_map_t *map, chunk_t *chunk);
+void remove_chunk(chunk_map_t *map, usize x, usize z);
+
+chunk_map_node_t *chunk_lookup(chunk_map_t *map, usize x, usize z);
+bool is_chunk_loaded(chunk_map_t *map, usize x, usize z);
+
+void get_all_chunks(chunk_map_t *map, chunk_t **chunk_buf, usize *count);
+void query_chunk_map(chunk_map_t *map, chunk_t **chunk_buf, usize *count, query_func func);
+
+#endif
